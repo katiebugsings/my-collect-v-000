e4a004b601347e4735e98d6e6efcadde38ff5a39
Katie Cassell <kcassell944@gmail.com>
2018-02-20 23:39:04 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,5 @@
-def my_collect
+def my_collect(array)
   array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 end
-my_collect(array) do |name|
-  name.split(" ").first
-end
+# my_collect(array) do |name|
+#   name.split(" ").first
